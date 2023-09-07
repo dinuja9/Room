26ccd56976ac6bb3365ecc6cf39193c57fd4eee3
dinuj9 <dinuja.w9@gmail.com>
2023-09-07 12:01:33 -0400

    added in the basics for opening and closing the modal

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.querySelector(".modal");
+    const openButton = document.querySelector(".get-started");
+    const closeButton = document.querySelector(".close-button");
+  
+    openButton.addEventListener("click", () => {
+      modal.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      modal.close();
+    });
+  });
