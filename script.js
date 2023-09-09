64c0e5c98ba7ab638134f3729a636a62af8ae28e
dinuj9 <dinuja.w9@gmail.com>
2023-09-09 07:43:31 -0400

    nothing new

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const modal = document.querySelector(".modal");
     const openButton = document.querySelector(".get-started");
     const closeButton = document.querySelector(".close-button");
-  
+
     openButton.addEventListener("click", () => {
       modal.showModal();
     });
-  
     closeButton.addEventListener("click", () => {
       modal.close();
     });
-  });
+});
